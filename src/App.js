2b0625ec755cf1cf17d609431fdc8aee6194b704
EduardRo <eduardstancu@yahoo.com>
2023-03-13 18:51:59 +0200

    save84

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
 
 import React from 'react';
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component'
 
 const App=()=> {
-  // initialize some array
-  const categories = [
-    {
-      id:1,
-      title:'Shirts',
-      imageUrl:'https://merer.ro/wp-content/uploads/2021/09/merer-desktop-poza-04-traditionale.jpg',
-      
-    },
-    {
-      id:2,
-      title:'Perfumes',
-      imageUrl:'https://merer.ro/wp-content/uploads/2021/12/amaze-him-ajmal-merer.ro_-1024x1024.jpg',
-      
-    },
-    {
-      id:3,
-      title:'Bracelets',
-      imageUrl:'https://merer.ro/wp-content/uploads/2019/03/22-8-1024x1024.jpg',
-      
-    },
-    {
-      id:4,
-      title:'Women',
-      imageUrl:'https://merer.ro/wp-content/uploads/2022/08/camasa-femei-vascoza-merer.ro_-1024x1024.jpg',
-      
-    },
-    {
-      id:5,
-      title:'Men',
-      imageUrl:'https://merer.ro/wp-content/uploads/2022/09/camasa-din-bumbac-merer.ro_-1024x1024.jpg',
-      
-    },
-  ]
+  
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path = '/' element={<Home />} />
+      
+
+    </Routes>
+    
 
   );
 }
