9117759cac21b1f4cdbea8cabe191e9af2a29a99
EduardRo <eduardstancu@yahoo.com>
2023-03-13 11:51:57 +0200

    save76

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@
 
 function App() {
   return (
-    <div>Hello world</div>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>HATS</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>JACKETS</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>SNEAKERS</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>WOMEN</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>MEN</h2>
+          <p>Shop Now</p>
+        </div>
+    
+      </div>
+     
+    </div>
   );
 }
 
