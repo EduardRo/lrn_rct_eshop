87e9da3d4ce64f8889992689f152b185b773194c
EduardRo <eduardstancu@yahoo.com>
2023-03-19 11:48:15 +0200

    save101

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import { createAuthUserWithEmailAndPassword,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
 
 const defaultFormFields = {
-  displayName: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
 };
 
 const SignUpForm = () => {
@@ -13,50 +17,47 @@ const SignUpForm = () => {
   const { displayName, email, password, confirmPassword } = formFields;
   console.log(formFields);
 
-  const resetFormFields =()=>{
+  const resetFormFields = () => {
     setFormFields(defaultFormFields);
-
   };
 
-  const handleSubmit = async(event)=>{
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // conform that password match
-    if(password!==confirmPassword){
+    if (password !== confirmPassword) {
       alert("password don't match");
       return;
-
     }
 
     try {
-      const {user} = await createAuthUserWithEmailAndPassword(email, password);
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
       //console.log({user});
-    } catch(error){
-          if(error.code == 'auth/emai-already-in-use'){
-            alert('We have already an user with this email');
-          } else {
-            console.log('user creation error:' , error);
-          }
-          
+    } catch (error) {
+      if (error.code == "auth/emai-already-in-use") {
+        alert("We have already an user with this email");
+      } else {
+        console.log("user creation error:", error);
       }
-    
-
-
+    }
   };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormFields({...formFields,[name]:value});
+    setFormFields({ ...formFields, [name]: value });
     console.log(formFields);
   };
 
   return (
-    <>
+    
       <div>
         <h1>Sign UP with your email and password</h1>
         <form onSubmit={handleSubmit} className="form-input">
-          <label>Display Name</label>
+          {/* <label>Display Name</label>
           <input
             type="text"
             required
@@ -64,38 +65,50 @@ const SignUpForm = () => {
             name="displayName"
             value={displayName}
             autoComplete="name"
-          />
-          <label>Email</label>
-          <input
-            type="email"
+          /> */}
+          <FormInput
+            label="Display Name"
+            type="text"
             required
             onChange={handleChange}
-            name="email"
-            value={email}
-            autoComplete="email"
+            name="displayName"
+            value={displayName}
+            autoComplete="name"
           />
-          <label>Password</label>
-          <input
-            type="password"
+          
+          <FormInput
+            label="Email"
+            type="text"
             required
             onChange={handleChange}
-            name="password"
-            value={password}
-            autoComplete="password"
+            name="displayName"
+            value={displayName}
+            autoComplete="name"
           />
-          <label>Confirm Password</label>
-          <input
-            type="password"
+          
+          <FormInput
+            label="Password"
+            type="text"
             required
             onChange={handleChange}
-            name="confirmPassword"
-            value={confirmPassword}
-            autoComplete="new-password"
+            name="displayName"
+            value={displayName}
+            autoComplete="name"
+          />
+          
+          <FormInput
+            label="Confirm Password"
+            type="text"
+            required
+            onChange={handleChange}
+            name="displayName"
+            value={displayName}
+            autoComplete="name"
           />
           <button type="submit">Sign Up</button>
         </form>
       </div>
-    </>
+    
   );
 };
 
